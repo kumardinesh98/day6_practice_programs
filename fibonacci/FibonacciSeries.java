e398095ad549d49b85c617f6e26f9bb04f470d2f
kumardinesh98 <devadineshkumar98@gmail.com>
2021-12-06 20:08:30 -0800

    program to find fibonacci series

---
+++ fibonacci/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the count");
+		int count = sc.nextInt();
+		long previous = 0;
+		long present = 1;
+		long temp = 0;
+		sc.close();
+		System.out.print(previous + "\t" + present + "\t");
+		for (int i = 0; i <= count; i++) {
+			temp = previous + present;
+			previous = present;
+			present = temp;
+			System.out.print(temp + "\t");
+		}
+	}
+
+}
